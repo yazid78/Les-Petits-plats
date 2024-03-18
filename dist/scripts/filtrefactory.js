ebafb5eddbba9b5a05b5a0603aef625f2027cb1c
Yazid <yazid780@outlook.fr>
2024-03-18 13:42:10 +0100

    Co-authored-by: clement.sabattie@hotmail.fr <clement.sabattie@hotmail.fr>

---
+++ dist/scripts/filtrefactory.js
@@ -0,0 +1,38 @@
+class FiltreFactory {
+    generateOption(ingredients, appliances, ustensils) {
+        // Sélection des éléments select dans le DOM
+        const selectIngredients = document.getElementById("selectIngredients");
+        const selectAppareils = document.getElementById("selectAppareils");
+        const selectUstensiles = document.getElementById("selectUstensiles");
+        selectIngredients.innerHTML = '';
+        selectAppareils.innerHTML = '';
+        selectUstensiles.innerHTML = '';
+        // Génération des options pour les ingrédients
+        ingredients.forEach(ingredient => {
+            const option = document.createElement("option");
+            option.value = ingredient;
+            option.textContent = ingredient;
+            selectIngredients.appendChild(option);
+        });
+        // Génération des options pour les appareils
+        appliances.forEach(appliance => {
+            const option = document.createElement("option");
+            option.value = appliance;
+            option.textContent = appliance;
+            selectAppareils.appendChild(option);
+        });
+        // Génération des options pour les ustensiles
+        ustensils.forEach(ustensil => {
+            const option = document.createElement("option");
+            option.value = ustensil;
+            option.textContent = ustensil;
+            selectUstensiles.appendChild(option);
+        });
+        return {
+            appliance: appliances,
+            ingredient: ingredients,
+            ustensils: ustensils
+        };
+    }
+}
+//# sourceMappingURL=filtrefactory.js.map
--- scripts/searchservice.ts
+++ scripts/searchservice.ts
@@ -1,96 +1,27 @@
-class SearchService {
+import recipes from "../data/recipes.js";
+
+export class SearchService {
+  resultat : any;
+
+  constructor() {
+    this.resultat = recipes;
+  }
 
   search(motRecherche: string, filtresSelectionnes: IFiltre): IResultatRecherche {
-
-    let resultat = [
-      {
-        id: 1,
-        image: "Recette01.jpg",
-        name: "Limonade de Coco",
-        servings: 1,
-        ingredients: [
-          {
-            ingredient: "Lait de coco",
-            quantity: 400,
-            unit: "ml",
-          },
-          {
-            ingredient: "Jus de citron",
-            quantity: 2,
-          },
-          {
-            ingredient: "Crème de coco",
-            quantity: 2,
-            unit: "cuillères à soupe",
-          },
-          {
-            ingredient: "Sucre",
-            quantity: 30,
-            unit: "grammes",
-          },
-          {
-            ingredient: "Glaçons",
-          },
-        ],
-        time: 10,
-        description:
-          "Mettre les glaçons à votre goût dans le blender, ajouter le lait, la crème de coco, le jus de 2 citrons et le sucre. Mixer jusqu'à avoir la consistence désirée",
-        appliance: "Blender",
-        ustensils: ["cuillère à Soupe", "verres", "presse citron"],
-      },
-      {
-        id: 2,
-        image: "Recette02.jpg",
-        name: "Poisson Cru à la tahitienne",
-        servings: 2,
-        ingredients: [
-          {
-            ingredient: "Thon Rouge (ou blanc)",
-            quantity: 200,
-            unit: "grammes",
-          },
-          {
-            ingredient: "Concombre",
-            quantity: 1,
-          },
-          {
-            ingredient: "Tomate",
-            quantity: 2,
-          },
-          {
-            ingredient: "Carotte",
-            quantity: 1,
-          },
-          {
-            ingredient: "Citron Vert",
-            quantity: 5,
-          },
-          {
-            ingredient: "Lait de Coco",
-            quantity: 100,
-            unit: "ml",
-          },
-        ],
-        time: 60,
-        description:
-          "Découper le thon en dés, mettre dans un plat et recouvrir de jus de citron vert (mieux vaut prendre un plat large et peu profond). Laisser reposer au réfrigérateur au moins 2 heures. (Si possible faites-le le soir pour le lendemain. Après avoir laissé mariner le poisson, coupez le concombre en fines rondelles sans la peau et les tomates en prenant soin de retirer les pépins. Rayer la carotte. Ajouter les légumes au poissons avec le citron cette fois ci dans un Saladier. Ajouter le lait de coco. Pour ajouter un peu plus de saveur vous pouvez ajouter 1 à 2 cuillères à soupe de Crème de coco",
-        appliance: "Saladier",
-        ustensils: ["presse citron"],
-      },
-    ];
+    if (motRecherche.length < 3) {
+      return;
+    }
     //algo de recherche
     motRecherche = motRecherche.trim().toLowerCase();
-  
-    if (motRecherche.length >= 3) {
-      resultat = resultat.filter((recette) => recette.name.trim().toLowerCase().includes(motRecherche) 
-      || recette.ingredients.some(ingredient => ingredient.ingredient.trim().toLowerCase().includes(motRecherche)) 
+    this.resultat = this.resultat.filter((recette) => recette.name.trim().toLowerCase().includes(motRecherche)
+      || recette.ingredients.some(ingredient => ingredient.ingredient.trim().toLowerCase().includes(motRecherche))
       || recette.description.trim().toLowerCase().includes(motRecherche));
 
-      console.log("retour:", resultat);
-    }
+
+    //algo filtresSelectionnes
 
     return {
-      recettes: resultat,
+      recettes: this.resultat,
       filtres: {
         ustensils: ["cuillère à Soupe", "verres", "presse citron"],
         ingredient: [
